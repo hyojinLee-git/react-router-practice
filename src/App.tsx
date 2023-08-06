842f472cb8236c7f1aac2d3980d8bcd691be2c06
hyojinLeee-git <splato8899@gmail.com>
2023-08-06 20:38:17 +0900

    feat: post router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Layout from "layouts/Layout";
 import Login from "pages/Login";
 import Main from "pages/Main";
+import Post from "pages/post/Posts";
+import Posts from "pages/post/Posts";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -10,7 +12,9 @@ function App() {
             <Route element={<Layout />}>
                 <Route path="/" element={<Main />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/posts" element={<Posts />} />
             </Route>
+            <Route path="/posts/:id" element={<Post />} />
         </Routes>
     );
 }
