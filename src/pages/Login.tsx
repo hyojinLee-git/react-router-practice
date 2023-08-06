fa20ef550df65e808cec11028bc42e7078a15578
hyojinLeee-git <splato8899@gmail.com>
2023-08-06 20:34:59 +0900

    feat: login, main 페이지 추가

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+const Login: React.FC = () => {
+    return (
+        <div>
+            <h1>login page</h1>
+            <input />
+            <input />
+        </div>
+    );
+};
+
+export default Login;
